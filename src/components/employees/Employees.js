990cba9ea0739c39cf2806c588f0cfbb6dc138f4
mohammedH97 <102896991+mohammedH97@users.noreply.github.com>
2023-08-14 22:20:51 +0300

    state and useState

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -1,17 +1,30 @@
 import NewEmployee from './NewEmployee'
 import Employee from './Employee'
+import { useState } from 'react'
+
+
+const initialData =[
+  {id:1,name:'Sara ahmed',job:'designer',active:true},
+  {id:2,name:'Ali ahmed',job:'developer',active:false},
+  {id:3,name:'Salma mohammed',job:'developer',active:false},
+  {id:4,name:'Zaineb Ahmed',job:'designer',active:true}
+]
+
 
 const Employees = () =>{
+  const [data , setData] = useState(initialData)
+ 
 
-  const data =[
-    {id:1,name:'Sara ahmed',job:'designer',active:true},
-    {id:2,name:'Ali ahmed',job:'developer',active:false},
-    {id:3,name:'Salma mohammed',job:'developer',active:false},
-    {id:4,name:'Zaineb Ahmed',job:'designer',active:true}
-  ]
+  const editName = () => {
+        const newData = [...data]
+        newData[0].name = 'saat'
+        setData(newData)
+        
+  }
     return(
         <div className="employees">
           <NewEmployee/>
+          <button className='btn' onClick={editName}>edit</button>
           <div className='employees_list card'>
           <Employee name={data[0].name} job={data[0].job}/>
           <Employee name={data[1].name} job={data[1].job}/>
